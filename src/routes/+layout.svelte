e98fd02cd0841eb91f3f4bf06c28ea272859be28
Gleb Kotovsky <terrnitllc.work@gmail.com>
2024-09-30 12:48:03 +0500

    init & implement ui of coffee/milk tracker

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  import "../styles/app.css";
+</script>
+
+
+<div class="mx-auto bg-zinc-900 flex items-center justify-center w-full h-screen">
+    <div class="container">
+        <slot />
+    </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+
+<div class="flex gap-x-1.5 *:uppercase *:rounded-md *:gap-x-1 *:py-3 *:px-1.5 w-[500px]">
+    <div class="flex text-orange-400 bg-orange-500/20 w-[37%]">
+        <div class="text-orange-500/30">coffee</div>
+        <div>37%</div>
+    </div>
+    <div class="w-1 !p-0 my-1.5 hover:my-0.5 hover:w-2 duration-100 transition-all ease-in-out cursor-pointer rounded-sm bg-white"/>
+    <div class="flex flex-row-reverse text-white bg-zinc-500 w-[63%]">
+        <div class="text-zinc-300">milk</div>
+        <div>63%</div>
+    </div>
+</div>
